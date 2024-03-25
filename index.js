dac0c2bfec5fa7e82807505d909ce3baeb32eae2
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-03-25 18:05:28 -0400

    add boilerplate server code

---
+++ index.js
@@ -0,0 +1,20 @@
+// Import statements
+const express = require("express");
+const db = require("/config/connection");
+const routes = require("/routes");
+
+// Initialize Express server
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Add Express middleware
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(routes);
+
+// Connect to the Mongo database and start the Express server
+db.once("open", () => {
+    app.listen(PORT, () => {
+        console.log(`API server listening on port ${PORT}`);
+    })
+})
