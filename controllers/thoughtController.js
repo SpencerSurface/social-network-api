bec2270ea1729bd87cb4f493679cb9b256586784
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-03-26 14:54:02 -0400

    implement controller function getSingleThought()

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -12,5 +12,60 @@ module.exports = {
             console.error(err);
             return res.status(500).json(err);
         }
-    }
+    },
+    async getSingleThought(req, res) {
+        try {
+            // Find a single thought
+            const thought = await Thought.findOne({_id: req.params.thoughtId});
+            // If the specified thought doesn't exist, respond with a 404 error
+            if (!thought) {
+                return res.status(404).json({message: "No thought with that ID"});
+            }
+            // Respond with the thought
+            return res.json(thought);
+        } catch(err) {
+            console.error(err);
+            return res.status(500).json(err);
+        }
+    },
+    async createThought(req, res) {
+        try {
+
+        } catch(err) {
+            console.error(err);
+            return res.status(500).json(err);
+        }
+    },
+    async updateThought(req, res) {
+        try {
+
+        } catch(err) {
+            console.error(err);
+            return res.status(500).json(err);
+        }
+    },
+    async deleteThought(req, res) {
+        try {
+
+        } catch(err) {
+            console.error(err);
+            return res.status(500).json(err);
+        }
+    },
+    async addReaction(req, res) {
+        try {
+
+        } catch(err) {
+            console.error(err);
+            return res.status(500).json(err);
+        }
+    },
+    async removeReaction(req, res) {
+        try {
+
+        } catch(err) {
+            console.error(err);
+            return res.status(500).json(err);
+        }
+    },
 }
