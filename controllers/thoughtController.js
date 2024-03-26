1cc40e9198c7284fcee3740f069819f626725859
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-03-26 15:16:14 -0400

    implemented controller function createThought()

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -17,7 +17,7 @@ module.exports = {
         try {
             // Find a single thought
             const thought = await Thought.findOne({_id: req.params.thoughtId});
-            // If the specified thought doesn't exist, respond with a 404 error
+            // If the specified thought doesn't exist, respond with an error
             if (!thought) {
                 return res.status(404).json({message: "No thought with that ID"});
             }
@@ -30,7 +30,20 @@ module.exports = {
     },
     async createThought(req, res) {
         try {
-
+            // Create a new thought
+            const thought = await Thought.create(req.body);
+            // Add the thought to the specified user
+            const user = await User.findOneAndUpdate(
+                { username: req.body.username },
+                { $addToSet: { thoughts: thought._id } },
+                { new: true}
+            );
+            // If the specified user doesn't exist, respond with an error
+            if (!user) {
+                return res.status(404).json({message: "Thought created, but no user with that username"});
+            }
+            // Respond with the thought
+            return res.json(thought);
         } catch(err) {
             console.error(err);
             return res.status(500).json(err);
