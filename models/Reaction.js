1d495dc8274c132607a609b237b4584f080f7eb5
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-03-25 21:33:10 -0400

    implement Reaction model

---
+++ models/Reaction.js
@@ -0,0 +1,36 @@
+// Import
+const { Schema, Types } = require("mongoose");
+const { formatDate } = require("../utils/helpers");
+
+// Define the reaction schema
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: formatDate
+        }
+    },
+    {
+        // Include the getter method
+        toJSON: {getters: true},
+        // Don't automatically generate ids
+        id: false
+    }
+);
+
+// Export
+module.exports = reactionSchema;
